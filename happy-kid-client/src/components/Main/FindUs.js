108d6d517a0e26d242976b9066b78ebb7e47aa63
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-07-08 00:38:25 +0200

    version to deploy-calendar-findus-css

--- happy-kid-client/src/components/Main/FindUs.js
+++ happy-kid-client/src/components/Main/FindUs.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div style={{color:"red", fontWeight:"bold"}}>{text}</div>;
+
+
+const AnyReactComponent = ({ text }) => <div style={this.marker}><img style={this.markerIMG} src="../../logo.jpg" alt="marker" /></div>;
 
 class FindUs extends Component {
   static defaultProps = {
     center: {
-      lat: 59.95,
-      lng: 30.33
+      lat: 52.3580,
+      lng: 4.8686
     },
     zoom: 11
   };
+
+  marker = {
+    width: "50px",
+    height: "50px",
+  }
+
+  markerIMG = {
+    height: "100%",
+    width: "auto",
+    oveflow: "hidden"
+  }
+
+  contactDetails = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "flex-start",
+    padding: "20px",
+    width: "30%",
+    height: "50vh",
+    backgroundColor: "#7BCB56",
+    color: "#54008B"
+  }
 
   render() {
     console.log(process.env.key)
     return (
 
-      <div style={{ height: '50vh', width: '50%' }}>
+      <div style={{ height: '50vh', width: '50%', display: "flex" }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyBOKG2gf34JUOyAbRwm7e85EtuHCqN5FdA" }}
           defaultCenter={this.props.center}
@@ -28,6 +53,11 @@ class FindUs extends Component {
             text="Happy-Kid"
           />
         </GoogleMapReact>
+        <div style={this.contactDetails}>
+          <span><b>Address:</b> Amsterdamweg 1 </span>
+          <span><b>e-mail:</b> happykid@gmail.com </span>
+          <span><b>Phone:</b> xxx-xx-xxx</span>
+        </div>
       </div>
     );
   }
