af1d04871739703e5fc487777edb62aaa2052c03
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-07-08 17:49:27 +0200

    version to deploy-adminpage-ul

--- happy-kid-client/src/components/Admin/AdminHomePage.js
+++ happy-kid-client/src/components/Admin/AdminHomePage.js
@@ -4,10 +4,10 @@ export default class AdminHomePage extends Component {
   render() {
     return (
       <div>
-        <fieldset>
+        <fieldset style={{height: "400px", display: "flex", flexDirection:"column", alignItems: "flex-start", padding: "20px", justifyContent:"space-around"}}>
           <legend>HappyKid &#x10E6;</legend>
           <h2>Hello, let's do some nice things for our parents and kids  &#x10E6;</h2>
-          <ul>
+          <ul style={{ listStyleType: "none"}}>
             <li>You can add new child in ADD-KID section</li>
             <li>You can connect parent with kid(s) in CONNECT section</li>
             <li>You can sen a message in SEND MESSAGE section</li>
@@ -19,3 +19,4 @@ export default class AdminHomePage extends Component {
     )
   }
 }
+
